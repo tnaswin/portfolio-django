0bb7ba91e14286a4e725a4f7159f0ceea0bf90e3
tnaswin <tnaswin007@gmail.com>
2020-05-19 18:09:52 +0530

    Setting up the Django Template and designing using Bootstrap

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -5,3 +5,7 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def detail(request, job_id):
+    print(job_id)
+    return render(request, 'jobs/home.html')
